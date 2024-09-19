611f52a7eb30764162983fc6749b250f1d12843c
Mok Jun Neng <jun-neng.mok@external.thalesgroup.com>
2024-09-19 19:03:46 +0800

    messaging session - still trying..

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,15 +1,10 @@
-use std::{
-    env,
-    io::Error as IoError,
-    net::SocketAddr,
-    sync::{Arc, RwLock},
-};
+use std::{env, io::Error as IoError, net::SocketAddr, sync::Arc};
 
-use futures_util::StreamExt;
+use futures_util::{future, pin_mut, SinkExt, StreamExt, TryStreamExt};
 
 use tokio::{
     net::{TcpListener, TcpStream},
-    sync::oneshot,
+    sync::{mpsc, oneshot, RwLock},
 };
 
 use bgapp::{
@@ -18,33 +13,51 @@ use bgapp::{
     repository::message::MessageRepository,
     services::messaging::{Command, MessagingService},
 };
+use tokio_stream::wrappers::BroadcastStream;
+use tokio_tungstenite::tungstenite::Message;
 
 // TODO: Push notification service to alert new messages
 // TODO: DTO and model for message
 
 #[tokio::main]
 async fn main() -> Result<(), IoError> {
-    // initiate database client connection
-    // TODO: May need to arc mutex this shit
-    let db_client = PrismaClient::_builder()
-        .build()
-        .await
-        .expect("Failed to initialize db client");
-    let message_repo = MessageRepository::new(db_client);
-    let messaging_service = Arc::new(RwLock::new(MessagingService::new(message_repo)));
-    messaging_service.clone().write().unwrap().start().await;
+    // Create mpsc channel and spawn tokio task to handle commands
+    let (commands_tx, mut commands_rx) = mpsc::channel::<Command>(32);
+    tokio::spawn(async move {
+        let db_client = PrismaClient::_builder()
+            .build()
+            .await
+            .expect("Failed to initialize db client");
+        let message_repo = MessageRepository::new(db_client);
 
+        while let Some(cmd) = commands_rx.recv().await {
+            match cmd {
+                Command::SaveMessage { message, resp } => {
+                    let _res = message_repo.upsert_many(vec![message]).await;
+                    let _ = resp.send("success!".to_string());
+                }
+            }
+        }
+    });
+
+    let messaging_service = Arc::new(RwLock::new(MessagingService::new()));
+
+    // Handle all incoming WS connections
     let addr = env::args()
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:8080".to_string());
-
     // Create the event loop and TCP listener we'll accept connections on.
     let listener = TcpListener::bind(&addr).await.expect("Failed to bind");
     println!("Listening on: {}", addr);
 
     // Spawn the handling of each connection in a separate task.
     while let Ok((stream, addr)) = listener.accept().await {
-        tokio::spawn(handle_connection(stream, addr, messaging_service.clone()));
+        tokio::spawn(handle_connection(
+            stream,
+            addr,
+            Arc::clone(&messaging_service),
+            commands_tx.clone(),
+        ));
     }
 
     Ok(())
@@ -54,27 +67,100 @@ async fn handle_connection(
     raw_stream: TcpStream,
     addr: SocketAddr,
     messaging_service: Arc<RwLock<MessagingService>>,
+    commands_tx: mpsc::Sender<Command>,
 ) {
     println!("Incoming TCP connection from: {}", addr);
     let ws_stream = tokio_tungstenite::accept_async(raw_stream)
         .await
         .expect("Error during the websocket handshake occurred");
     println!("WebSocket connection established: {}", addr);
-    let (outgoing, mut incoming) = ws_stream.split();
+    let (mut outgoing, mut incoming) = ws_stream.split();
+
     let first_msg = incoming.next().await.unwrap().unwrap();
     let init_message: MessageDto = serde_json::from_slice(&first_msg.into_data()).unwrap();
     println!("Received init message {:?}", init_message);
 
-    let msg_service_tx = messaging_service.read().unwrap().get_tx_channel();
-    tokio::spawn(async move {
+    // TODO: Default registration of one on one chat now; Handle group chat too
+    println!("Retrieving write lock to register chat");
+
+    // Get write lock to register chat
+    let chat_session_id;
+    {
+        println!("Getting write lock on messaging service to register chat.");
+        let mut messaging_service_write_lock = messaging_service.write().await;
+        chat_session_id = messaging_service_write_lock
+            .register_one_on_one_chat(init_message.sender_id, init_message.recipient_id);
+    }
+
+    let sender_id = init_message.sender_id;
+    let broadcast_targets;
+    let sender_rx;
+    {
+        let messaging_service_read_lock = messaging_service.read().await;
+        let chat_session = messaging_service_read_lock.get_chat_session(chat_session_id);
+        broadcast_targets = chat_session.get_broadcast_channels(sender_id);
+        sender_rx = chat_session.get_user_receiving_channel(sender_id);
+    }
+
+    // let broadcast_incoming = incoming.try_for_each(|msg| async move {
+    //     println!(
+    //         "Received a message from {}: {}",
+    //         &sender_id,
+    //         &msg.to_text().unwrap()
+    //     );
+    //
+    //     broadcast_targets.into_iter().for_each(|target| {
+    //         target
+    //             .send(Message::Binary(msg.clone().into_data()))
+    //             .unwrap();
+    //     });
+    //
+    //     let message: MessageDto =
+    //         serde_json::from_slice(&msg.into_data()).expect("Failed to deserialize message");
+    //
+    //     // save message
+    //     let (resp_tx, resp_rx) = oneshot::channel();
+    //     let cmd = Command::SaveMessage {
+    //         message,
+    //         resp: resp_tx,
+    //     };
+    //     commands_tx.send(cmd).await.unwrap();
+    //
+    //     let res = resp_rx.await;
+    //     println!("Received response from SaveMessage command: {:?}", res);
+    //     Ok(())
+    // });
+    let receive_from_others = BroadcastStream::new(sender_rx).try_for_each(|msg| {
+        let _ = outgoing.send(msg);
+        future::ok(())
+    });
+    let _ = receive_from_others.await;
+
+    while let msg = incoming.next().await.unwrap().unwrap() {
+        println!(
+            "Received a message from {}: {}",
+            &sender_id,
+            &msg.to_text().unwrap()
+        );
+
+        broadcast_targets.iter().for_each(|target| {
+            target
+                .send(Message::Binary(msg.clone().into_data()))
+                .unwrap();
+        });
+
+        let message: MessageDto =
+            serde_json::from_slice(&msg.into_data()).expect("Failed to deserialize message");
+
+        // save message
         let (resp_tx, resp_rx) = oneshot::channel();
         let cmd = Command::SaveMessage {
-            message: init_message,
+            message,
             resp: resp_tx,
         };
-        msg_service_tx.send(cmd).await.unwrap();
+        commands_tx.send(cmd).await.unwrap();
 
         let res = resp_rx.await;
-        println!("GOT = {:?}", res);
-    });
+        println!("Received response from SaveMessage command: {:?}", res);
+    }
 }
--- src/services/messaging.rs
+++ src/services/messaging.rs
@@ -1,21 +1,9 @@
 use std::collections::HashMap;
 
-use futures_util::{
-    future, pin_mut,
-    stream::{SplitSink, SplitStream},
-    Future, SinkExt, StreamExt, TryStreamExt,
-};
-use tokio::{
-    net::TcpStream,
-    sync::{
-        broadcast::{self, Receiver, Sender},
-        mpsc, oneshot,
-    },
-};
-use tokio_stream::wrappers::BroadcastStream;
-use tokio_tungstenite::{tungstenite::Message, WebSocketStream};
+use tokio::sync::{broadcast, oneshot};
+use tokio_tungstenite::tungstenite::Message;
 
-use crate::{dtos::message::MessageDto, repository::message::MessageRepository};
+use crate::dtos::message::MessageDto;
 
 type Responder<T> = oneshot::Sender<T>;
 #[derive(Debug)]
@@ -27,158 +15,97 @@ pub enum Command {
     },
 }
 
+type UserId = u32;
+type ChatId = u32;
+type Channel = (broadcast::Sender<Message>, broadcast::Receiver<Message>);
+
+pub struct ChatSession {
+    id: ChatId,
+    users: Vec<UserId>,
+    channels: HashMap<UserId, Channel>,
+}
+impl ChatSession {
+    pub fn new(id: ChatId, users: Vec<UserId>) -> ChatSession {
+        let mut channels = HashMap::new();
+        for user in users.iter() {
+            let (tx, rx) = broadcast::channel(2);
+            channels.insert(*user, (tx, rx));
+        }
+        ChatSession {
+            id,
+            users,
+            channels,
+        }
+    }
+
+    pub fn get_user_receiving_channel(&self, user_id: UserId) -> broadcast::Receiver<Message> {
+        let (tx, _) = self.channels.get(&user_id).unwrap();
+        tx.subscribe()
+    }
+
+    // Return all users' channels except the sender's channel to broadcast into
+    pub fn get_broadcast_channels(&self, user_id: UserId) -> Vec<broadcast::Sender<Message>> {
+        let mut channels = vec![];
+        for user in self.users.iter() {
+            // skip sender
+            if *user != user_id {
+                let (tx, _) = self.channels.get(&user).unwrap();
+                channels.push(tx.clone());
+            }
+        }
+        channels
+    }
+}
+
 pub struct MessagingService {
-    message_repo: MessageRepository,
-    commands_tx: mpsc::Sender<Command>,
-    commands_rx: mpsc::Receiver<Command>,
+    next_chat_id: ChatId,
+    // TechDebt: is there a better data structure?
+    one_on_one_chat_registry: HashMap<(UserId, UserId), ChatId>,
+    chat_sessions: HashMap<ChatId, ChatSession>,
 }
 
 impl MessagingService {
-    pub fn new(message_repo: MessageRepository) -> Self {
-        // TODO: Define proper channel capacity
-        let (tx, rx) = mpsc::channel(32);
+    pub fn new() -> Self {
         Self {
-            message_repo,
-            commands_tx: tx,
-            commands_rx: rx,
+            next_chat_id: 1,
+            one_on_one_chat_registry: HashMap::new(),
+            chat_sessions: HashMap::new(),
         }
     }
 
-    pub fn get_tx_channel(&self) -> mpsc::Sender<Command> {
-        self.commands_tx.clone()
-    }
-
-    pub async fn start(&mut self) {
-        while let Some(cmd) = self.commands_rx.recv().await {
-            match cmd {
-                Command::SaveMessage { message, resp } => {
-                    let _res = self.message_repo.upsert_many(vec![message]).await;
-                    // TODO: Return proper DB response
-                    let _ = resp.send("success!".to_string());
-                }
+    pub fn register_one_on_one_chat(&mut self, user1_id: UserId, user2_id: UserId) -> ChatId {
+        match self.one_on_one_chat_registry.get(&(user1_id, user2_id)) {
+            Some(chat_id) => {
+                println!(
+                    "Found existing chat between {} and {}. Skip registration.",
+                    &user1_id, &user2_id
+                );
+                *chat_id
             }
+            None => match self.one_on_one_chat_registry.get(&(user2_id, user1_id)) {
+                Some(chat_id) => {
+                    println!(
+                        "Found existing chat between {} and {}. Skip registration.",
+                        &user1_id, &user2_id
+                    );
+                    *chat_id
+                }
+                None => {
+                    self.one_on_one_chat_registry
+                        .insert((user1_id, user2_id), self.next_chat_id);
+                    let chat_session =
+                        ChatSession::new(self.next_chat_id, vec![user1_id, user2_id]);
+                    let chat_session_id = self.next_chat_id;
+                    self.next_chat_id += 1;
+                    self.chat_sessions.insert(chat_session.id, chat_session);
+                    println!("Registered chat for {} and {}", &user1_id, &user2_id);
+                    chat_session_id
+                }
+            },
         }
+    }
+
+    pub fn get_chat_session(&self, chat_id: ChatId) -> &ChatSession {
+        self.chat_sessions.get(&chat_id).unwrap()
     }
 }
-
-// type UserId = u32;
-// type ChatId = u32;
-// type Channel = (Sender<Message>, Receiver<Message>);
-//
-// pub struct ChatSession {
-//     id: ChatId,
-//     users: Vec<UserId>,
-//     channels: HashMap<UserId, Channel>,
-// }
-// impl ChatSession {
-//     pub fn new(id: ChatId, users: Vec<UserId>) -> ChatSession {
-//         let mut channels = HashMap::new();
-//         for user in users.iter() {
-//             let (tx, rx) = broadcast::channel(2);
-//             channels.insert(*user, (tx, rx));
-//         }
-//         ChatSession {
-//             id,
-//             users,
-//             channels,
-//         }
-//     }
-//
-//     pub fn get_user_receiving_channel(&self, user_id: UserId) -> Receiver<Message> {
-//         let (tx, _) = self.channels.get(&user_id).unwrap();
-//         tx.subscribe()
-//     }
-//
-//     // Return all users' channels except the sender's channel to broadcast into
-//     pub fn get_broadcast_channels(&self, user_id: UserId) -> Vec<&Sender<Message>> {
-//         self.users
-//             .clone()
-//             .into_iter()
-//             .filter(|id| *id != user_id)
-//             .map(|id| &self.channels.get(&id).unwrap().0)
-//             .collect()
-//     }
-// }
-//
-// pub struct MessagingService {
-//     message_repo: MessageRepository,
-//     next_chat_id: ChatId,
-//     // TechDebt: is there a better data structure?
-//     one_on_one_chat_registry: HashMap<(UserId, UserId), ChatId>,
-//     chat_sessions: HashMap<ChatId, ChatSession>,
-// }
-//
-// impl MessagingService {
-//     pub fn new(message_repo: MessageRepository) -> Self {
-//         Self {
-//             message_repo,
-//             next_chat_id: 1,
-//             one_on_one_chat_registry: HashMap::new(),
-//             chat_sessions: HashMap::new(),
-//         }
-//     }
-//
-//     pub fn register_one_on_one_chat(&mut self, user1_id: UserId, user2_id: UserId) -> ChatId {
-//         match self.one_on_one_chat_registry.get(&(user1_id, user2_id)) {
-//             Some(chat_id) => *chat_id,
-//             None => match self.one_on_one_chat_registry.get(&(user2_id, user1_id)) {
-//                 Some(chat_id) => *chat_id,
-//                 None => {
-//                     let chat_session =
-//                         ChatSession::new(self.next_chat_id, vec![user1_id, user2_id]);
-//                     let chat_session_id = self.next_chat_id;
-//                     self.next_chat_id += 1;
-//                     self.chat_sessions.insert(chat_session.id, chat_session);
-//                     chat_session_id
-//                 }
-//             },
-//         }
-//     }
-//
-//     pub async fn start_live_chat(
-//         &self,
-//         mut outgoing: SplitSink<WebSocketStream<TcpStream>, Message>,
-//         incoming: SplitStream<WebSocketStream<TcpStream>>,
-//         chat_session_id: ChatId,
-//         sender_id: UserId,
-//     ) {
-//         let Some(chat_session) = self.chat_sessions.get(&chat_session_id) else {
-//             println!("Chat {} has not been registered.", chat_session_id);
-//             return;
-//         };
-//
-//         let broadcast_incoming = incoming.try_for_each(|msg| async {
-//             println!(
-//                 "Received a message from {}: {}",
-//                 &sender_id,
-//                 &msg.to_text().unwrap()
-//             );
-//
-//             let broadcast_targets = chat_session.get_broadcast_channels(sender_id);
-//             broadcast_targets.into_iter().for_each(|target| {
-//                 target
-//                     .send(Message::Binary(msg.clone().into_data()))
-//                     .unwrap();
-//             });
-//
-//             let message: MessageDto =
-//                 serde_json::from_slice(&msg.into_data()).expect("Failed to deserialize message");
-//             // save message in chat history
-//             self.save_message(message).await;
-//             Ok(())
-//         });
-//
-//         let sender_rx = BroadcastStream::new(chat_session.get_user_receiving_channel(sender_id));
-//         let receive_from_others = sender_rx.try_for_each(|msg| {
-//             outgoing.send(msg);
-//             future::ok(())
-//         });
-//
-//         pin_mut!(broadcast_incoming, receive_from_others);
-//         future::select(broadcast_incoming, receive_from_others).await;
-//     }
-//
-//     async fn save_message(&self, message: MessageDto) {
-//         self.message_repo.upsert_many(vec![message]).await;
-//     }
-// }
